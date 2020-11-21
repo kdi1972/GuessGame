60143dd7c8371d99d58f4f7ede9e4ac96647c4df
Kalenchik Dmitriy@kdi1972 <dmitriykalenchik@gmail.com>
2020-11-21 21:57:18 +0300

    first commit

---
+++ src/com/company/GameLauncher.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class GameLauncher {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Начинаем проект");
+    }
+}
